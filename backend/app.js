e95fcb415de6203fb2a41e4757a73c66362b0b92
ALI_Zucker <zabihiali1998@gmail.com>
2025-05-16 21:53:59 +0330

    first push

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {homeRouter} = require('./router/homeRouter');
+const {newsRouter} = require("./router/newsRouter");
+const {loginRouter}=require("./router/loginUser")
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/home", homeRouter);
+app.use("/news",newsRouter);
+app.use("/user",loginRouter);
+
+app.get('/', async (req, res) => {
+    res.send("Welcome ");
+});
+
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
